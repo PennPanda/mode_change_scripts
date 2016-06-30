44c07ce0f4f8034be54bfb9ac3b0e931e8328d09
Tianyang Chen <tianyangpenn@gmail.com>
2016-06-30 03:24:34 -0400

    added test loop and sort

--- proc_trace.sh
+++ proc_trace.sh
@@ -1,23 +1,32 @@
 #/bin/bash
 
-cat trace.bin | xentrace_format ~/event_driven/event_driven/tools/xentrace/formats | grep "rtds" > trace_proc_output/rtds
+trace_proc_output=$1
+echo saving this trace to $1
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:job_release"' > trace_proc_output/release_job.txt
+cat trace.bin | xentrace_format ~/event_driven/event_driven/tools/xentrace/formats | grep "rtds" > $trace_proc_output/temp
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:schedule"' > trace_proc_output/schedule.txt
+echo soriting
+sort -k 2 -n $trace_proc_output/temp > $trace_proc_output/rtds
+echo removing
+rm $trace_proc_output/temp
+echo partiing
+
+cat $trace_proc_output/rtds | awk '$5 == "rtds:job_release"' > $trace_proc_output/release_job.txt
+
+cat $trace_proc_output/rtds | awk '$5 == "rtds:schedule"' > $trace_proc_output/schedule.txt
  
-cat trace_proc_output/rtds | awk '$5 == "rtds:dis_running"' > trace_proc_output/dis_running.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:dis_running"' > $trace_proc_output/dis_running.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:dis_not_running"' > trace_proc_output/dis_not_running.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:dis_not_running"' > $trace_proc_output/dis_not_running.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:vcpu_enable"' > trace_proc_output/vcpu_enable.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:vcpu_enable"' > $trace_proc_output/vcpu_enable.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:mcr"' > trace_proc_output/mcr.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:mcr"' > $trace_proc_output/mcr.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:job_queued"' > trace_proc_output/job_queued.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:job_queued"' > $trace_proc_output/job_queued.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:update_changed"' > trace_proc_output/update_changed.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:update_changed"' > $trace_proc_output/update_changed.txt
 
-cat trace_proc_output/rtds | awk '$5 == "rtds:backlog_satisfied"' > trace_proc_output/backlog.txt
+cat $trace_proc_output/rtds | awk '$5 == "rtds:backlog_satisfied"' > $trace_proc_output/backlog.txt
 
 #scp trace_proc_output/*.txt tim@home:~/modechange/
--- run_trace.sh
+++ run_trace.sh
@@ -1,20 +1,24 @@
 #/bin/bash
 
+for ((i=0;i<50;i++))
+do
 
-
-sudo xentrace -D -e 0x0002f000 trace.bin &
 sleep 0.01
 #./changed
 ./disable_all
 sleep 0.01
 ./enable_all
-sleep 0.03
+sleep 0.01
+sudo xentrace -D -e 0x0002f000 trace.bin &
+sleep 0.02
 ./test
 sleep 0.07
 
 sudo killall xentrace
 
-./proc_trace.sh
+./proc_trace.sh trace_proc_output/test$i
+done
+
 #xenalyze --scatterplot-pcpu trace.bin | grep ^1v > p.txt
 #sed -i 's/1v//g' p.txt
 
